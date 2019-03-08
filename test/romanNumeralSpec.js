8a00d6c55fb993f26425fc80e759854b309be660
nwoodard-cs <nwoodard.cs@gmail.com>
2019-03-08 16:41:42 -0700

    Removed HTTP tests

--- test/romanNumeralSpec.js
+++ test/romanNumeralSpec.js
@@ -1,87 +1,73 @@
-const assert = require('assert')
+const should = require('chai').should()
 const romanNumeral = require('../app/routes/romanNumeral')
 
-describe('RomanNumeral', () => {
+describe('romanNumeral', () => {
 
-  
-  describe('romanNumeral.routes', () => {
-    it('Should have a response code of 200 on success')
-    // it('Should have a response code of 404 on invalid URI path') TODO: this is in app.js land, not here
-    it('Should have a response code of 400 on invalid parameter name')
-    it('Should, have a response code of 400 on invalid parameter value')
-    it('Should generate a response within 50ms')
-
-  })
-
-
-  // Converts an integer into a roman numeral
-  // number (integer) => romanNumeral [string], callback(err,res)
   describe('convertToRoman', () => {
     // Check boundaries
     it('Should return I for an input of 1', (done) => {
       romanNumeral.convertToRoman(1, (err, result) => {
-        assert.equal(result, 'I')
+        should.not.exist(err)
+        result.should.equal('I')
         done()
       })
     })
 
     it ('Should return MMMCMXCVII for an input of 3999', (done) => {
       romanNumeral.convertToRoman(3999, (err, result) => {
-        assert.equal(result, 'MMMCMXCIX')
+        should.not.exist(err)
+        result.should.equal('MMMCMXCIX')
         done()
       })
     })
 
   }) // End describe(convertToRoman)
 
-
-  // Validates that input is a positive integer within range.
-  // number (integer?) => callback(err)
-  // err will be null if number is valid
   describe('checkInput', () => {
-    
+    // Ensure errors are being reported
     it('Should return an error message on noninteger input', (done) => {
       romanNumeral.checkInput('garbage', err => {
-        assert.notEqual(err, null)
+        should.exist(err)
         done()
       })
     })
 
     it('Should return an error message if request is greater than MAX_VALUE', (done) => {
       romanNumeral.checkInput((romanNumeral.MAX_VALUE + 1), err => {
-        assert.notEqual(err, null)
+        should.exist(err)
         done()
       })
     })
 
     it('Should return an error message if the integer is 0', (done) => {
       romanNumeral.checkInput(0, err => {
-        assert.notEqual(err, null)
+        should.exist(err)
         done()
       })
     })
 
     it('Should return an error message if the integer is negative', (done) => {
       romanNumeral.checkInput(-3400, err => {
-        assert.notEqual(err, null)
+        should.exist(err)
         done()
       })
     })
 
-    it('Should return null if the integer is valid', (done) => {
+    it('Should not return error if the integer is valid', (done) => {
       romanNumeral.checkInput(27, err => {
-        assert.equal(err, null)
+        should.not.exist(err)
         done()
       })
     })
 
+    // Function should exhibit same behavior for int and string
     it('Should return the same result for valid integer and equivalent string parameters', (done) => {
       const passCaseInt = 45
       const passCaseStr = '45'
 
       romanNumeral.checkInput(passCaseInt, intErr => {
         romanNumeral.checkInput(passCaseStr, strErr => {
-          assert.equal(intErr, strErr)
+          should.equal(intErr, strErr)
           done()
         })
       })
@@ -93,7 +79,7 @@ describe('RomanNumeral', () => {
 
       romanNumeral.checkInput(failCaseInt, intErr => {
         romanNumeral.checkInput(failCaseStr, strErr => {
-          assert.equal(intErr, strErr)
+          intErr.should.equal(strErr)
           done()
         })
       })
